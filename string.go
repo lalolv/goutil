e52e9477aa4c8ba20291cb032f54ff06130a52bf
Lalo <code@lalo.im>
2020-07-08 11:10:27 +0800

    格式化显示字节和时长

--- string.go
+++ string.go
@@ -1,6 +1,7 @@
 package goutil
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -119,4 +120,32 @@ func EscapeQuots(val string) string {
 	str = strings.Replace(str, `"`, `\"`, -1)
 
 	return str
+}
+
+// DurationFormat returns a human-readable string representation of nanosecond number
+func DurationFormat(ns int64) string {
+	suffix := "s"
+	num := float64(ns)
+	units := []string{"n", "u", "m"}
+	for _, unit := range units {
+		if num < 1000.0 {
+			return fmt.Sprintf("%3.1f%s%s", num, unit, suffix)
+		}
+		num = (num / 1000)
+	}
+	return fmt.Sprintf("%.1f%s%s", num, "", suffix)
+}
+
+// ByteFormat returns a human-readable string representation of a byte count
+func ByteFormat(size int64) string {
+	suffix := "B"
+	num := float64(size)
+	units := []string{"", "K", "M", "G", "T", "P", "E", "Z"} // "Y" caught  below
+	for _, unit := range units {
+		if num < 1024.0 {
+			return fmt.Sprintf("%3.1f%s%s", num, unit, suffix)
+		}
+		num = (num / 1024)
+	}
+	return fmt.Sprintf("%.1f%s%s", num, "Y", suffix)
 }
